30d216d505c91dc3d57624b45133487699009395
Stephen Bird <sebirdman@gmail.com>
2023-08-19 19:35:26 -0700

    Initial Proof of concept for upgrade package generator

---
+++ br2miyoomini/package/upgrade_blob/mxpgenerator_generator.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+IPL=`stat -c "%s" ./input/ipl`
+IPL_CUST=`stat -c "%s" ./input/ipl_cust`
+MXPT=`stat -c "%s" ./input/mxpt`
+UBOOT=`stat -c "%s" ./input/uboot`
+UBOOT_ENV=`stat -c "%s" ./input/uboot_env`
+
+
+KERNEL=`stat -c "%s" ./input/kernel`
+KEY_CUST=`stat -c "%s" ./input/key_cust`
+LOGO=`stat -c "%s" ./input/logo`
+ROOTFS=`stat -c "%s" ./input/rootfs`
+MISERVICE=`stat -c "%s" ./input/miservice`
+CUSTOMER=`stat -c "%s" ./input/customer`
+APPCONFIGS=`stat -c "%s" ./input/appconfigs`
+
+declare -x IPL_HEX=0x10000 #`printf "0x%x\n" $IPL`
+declare -x IPL_CUST_HEX=0x10000 #`printf "0x%x\n" $IPL_CUST`
+declare -x MXPT_HEX=0x10000 #`printf "0x%x\n" $MXPT`
+declare -x UBOOT_HEX=0x2E000 #`printf "0x%x\n" $UBOOT`
+declare -x UBOOT_ENV_HEX=0x2000 #`printf "0x%x\n" $UBOOT_ENV`
+
+declare -x KERNEL_HEX=`printf "0x%x\n" $KERNEL`
+declare -x KEY_CUST_HEX=`printf "0x%x\n" $KEY_CUST`
+declare -x LOGO_HEX=`printf "0x%x\n" $LOGO`
+declare -x ROOTFS_HEX=`printf "0x%x\n" $ROOTFS`
+declare -x MISERVICE_HEX=`printf "0x%x\n" $MISERVICE`
+declare -x CUSTOMER_HEX=`printf "0x%x\n" $CUSTOMER`
+declare -x APPCONFIGS_HEX=`printf "0x%x\n" $APPCONFIGS`
+
+
+./mxpgenerator \
+    "$IPL_HEX(IPL),$IPL_CUST_HEX(IPL_CUST),$MXPT_HEX(MXPT),$UBOOT_HEX(UBOOT),$UBOOT_ENV_HEX(UBOOT_ENV)" \
+    "$KERNEL_HEX(KERNEL),$KEY_CUST_HEX(KEY_CUST),$LOGO_HEX(LOGO),$ROOTFS_HEX(ROOTFS),$MISERVICE_HEX(MISERVICE),$CUSTOMER_HEX(CUSTOMER),$APPCONFIGS_HEX(APPCONFIGS)" \
+    ./input/mxpt
+
+
+genimage --config boot.cfg
+
+BOOT=`stat -c "%s" ./images/boot.img`
+
+declare -x BOOT_HEX=`printf "0x%x\n" $BOOT`
+
+DATA_START=0x4000
+
+rm ./input/flash_script.txt
+
+echo "build:202305052130" >> ./input/flash_script.txt
+echo "" >> ./input/flash_script.txt
+
+# First we want to write the main bootloader info to the flash
+# This one is done slightly differently than the others, for good reason
+# the mxpt can change - and if it does, then we don't know what we're going to write to
+echo 'sf probe 0' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$BOOT_HEX $DATA_START" >> ./input/flash_script.txt
+
+echo 'sf erase 0 ${filesize}' >> ./input/flash_script.txt
+
+# Write the data in memory into the flash
+echo "sf write 0x21000000 0 $BOOT_HEX" >> ./input/flash_script.txt
+
+# We do this so we can load the new partition table
+echo 'mxp t.load' >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X" $(($DATA_START + $BOOT_HEX + $UBOOT_ENV_HEX))`
+
+###### KERNEL #######
+
+echo 'mxp r.info KERNEL' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$KERNEL_HEX $DATA_START" >> ./input/flash_script.txt
+
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$KERNEL_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $KERNEL_HEX))`
+
+###### KEY_CUST #######
+
+echo 'mxp r.info KEY_CUST' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$KEY_CUST_HEX $DATA_START" >> ./input/flash_script.txt
+
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$KEY_CUST_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $KEY_CUST_HEX))`
+
+###### LOGO #######
+
+echo 'mxp r.info LOGO' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$LOGO_HEX $DATA_START" >> ./input/flash_script.txt
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$LOGO_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $LOGO_HEX))`
+
+###### ROOTFS #######
+echo 'mxp r.info ROOTFS' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$ROOTFS_HEX $DATA_START" >> ./input/flash_script.txt
+
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$ROOTFS_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $ROOTFS_HEX))`
+
+###### MISERVICE #######
+echo 'mxp r.info MISERVICE' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$MISERVICE_HEX $DATA_START" >> ./input/flash_script.txt
+
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$MISERVICE_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $MISERVICE_HEX))`
+
+###### CUSTOMER #######
+echo 'mxp r.info CUSTOMER' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$CUSTOMER_HEX $DATA_START" >> ./input/flash_script.txt
+
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$CUSTOMER_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $CUSTOMER_HEX))`
+
+###### APPCONFIGS #######
+echo 'mxp r.info APPCONFIGS' >> ./input/flash_script.txt
+echo 'sf erase ${sf_part_start} ${sf_part_size}' >> ./input/flash_script.txt
+
+# Read the boot data from the image into memory
+printf "fatload mmc 0 0x21000000" >> ./input/flash_script.txt
+printf ' $(SdUpgradeImage) ' >> ./input/flash_script.txt
+echo "$APPCONFIGS_HEX $DATA_START" >> ./input/flash_script.txt
+
+printf "sf write 0x21000000" >> ./input/flash_script.txt
+printf ' ${sf_part_start} ' >> ./input/flash_script.txt
+echo "$APPCONFIGS_HEX" >> ./input/flash_script.txt
+
+DATA_START=`printf "0x%X\n" $(($DATA_START + $APPCONFIGS_HEX))`
+
+echo "saveenv" >> ./input/flash_script.txt
+echo "saveenv" >> ./input/flash_script.txt
+
+echo "reset" >> ./input/flash_script.txt
+
+
+
+
+genimage --config system.cfg
+
+genimage --config upgrade.cfg
